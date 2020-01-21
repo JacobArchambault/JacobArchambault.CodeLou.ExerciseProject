6b08762be7c6dc261b77cf779e12aa8a4f072a50
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-21 18:51:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JacobArchambault.CodeLou.ExerciseProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Student Id");
+            int studentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter First Name");
+            string studentFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string studentLastName = Console.ReadLine();
+            Console.WriteLine("Enter Class Name");
+            string className = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed");
+            string lastClass = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+            DateTimeOffset lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+            DateTimeOffset startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+            Student studentRecord = new Student
+            {
+                StudentId = studentId,
+                FirstName = studentFirstName,
+                LastName = studentLastName,
+                ClassName = className,
+                StartDate = startDate,
+                LastClassCompleted = lastClass,
+                LastClassCompletedOn = lastCompletedOn
+            };
+            Console.WriteLine($"Student Id | Name |  Class "); ;
+            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
+            Console.ReadKey();
+
+        }
+    }
+}
