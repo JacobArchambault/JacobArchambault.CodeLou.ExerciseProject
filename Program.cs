4b57a9383067789b3dbdf52265106ad8623f5ca6
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-22 12:05:07 -0500

    Change PrintRecord to a generic method which gets its typeInfo from object passed to it.

--- Program.cs
+++ Program.cs
@@ -8,17 +8,16 @@ namespace JacobArchambault.CodeLou.ExerciseProject
     {
         static void Main() 
         {
-            PrintRecord();
-            //List<Student> listOfStudents = new List<Student> { };
-            //int keepGoing;
-            //do 
-            //{
-            //    listOfStudents.Add(GetStudentInfoFromUser());
-            //    Console.WriteLine("Press 1 to enter another student record. Press any other key to exit the program");
-            //    _ = int.TryParse(Console.ReadLine(), out keepGoing);
-            //} while (keepGoing == 1);
+            List<Student> listOfStudents = new List<Student> { };
+            int keepGoing;
+            do
+            {
+                listOfStudents.Add(GetStudentInfoFromUser());
+                Console.WriteLine("Press 1 to enter another student record. Press any other key to exit the program");
+                _ = int.TryParse(Console.ReadLine(), out keepGoing);
+            } while (keepGoing == 1);
 
-            //listOfStudents.ForEach(s => PrintRecord(s));
+            listOfStudents.ForEach(s => PrintRecord(s));
         }
         static Student GetStudentInfoFromUser()
         {
@@ -54,12 +53,12 @@ namespace JacobArchambault.CodeLou.ExerciseProject
                 LastClassCompletedOn = lastCompletedOn
             };
         }
-        static void PrintRecord()
+        static void PrintRecord<T>(T record)
         {
-            PropertyInfo[] propertyInfo = typeof(Student).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo[] propertyInfo = record.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             foreach (var propInfo in propertyInfo)
                 Console.Write($"|\t {propInfo.Name} \t");
-            //Console.WriteLine($"Student Id \t| First Name \t| Last Name \t|  Class ");
+            Console.WriteLine();
             //Console.WriteLine($"{studentRecord.StudentId} \t| {studentRecord.FirstName} \t| {studentRecord.LastName} \t| {studentRecord.ClassName} ");
         }
     }
