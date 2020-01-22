c19ac9fce719678bf8ab45d76a5af0c8513368f1
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-21 19:25:13 -0500

    Add do-while loop to main method.

--- Program.cs
+++ Program.cs
@@ -6,34 +6,39 @@ namespace JacobArchambault.CodeLou.ExerciseProject
     {
         static void Main()
         {
-            Console.WriteLine("Enter Student Id");
-            int studentId = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter First Name");
-            string studentFirstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            string studentLastName = Console.ReadLine();
-            Console.WriteLine("Enter Class Name");
-            string className = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed");
-            string lastClass = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
-            DateTimeOffset lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
-            DateTimeOffset startDate = DateTimeOffset.Parse(Console.ReadLine());
-
-            Student studentRecord = new Student
+            int keepGoing;
+            do
             {
-                StudentId = studentId,
-                FirstName = studentFirstName,
-                LastName = studentLastName,
-                ClassName = className,
-                StartDate = startDate,
-                LastClassCompleted = lastClass,
-                LastClassCompletedOn = lastCompletedOn
-            };
-            Console.WriteLine($"Student Id | Name |  Class "); ;
-            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} "); ;
-            Console.ReadKey();
+                Console.WriteLine("Enter Student Id");
+                _ = int.TryParse(Console.ReadLine(), out int studentId);
+                Console.WriteLine("Enter First Name");
+                string studentFirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                string studentLastName = Console.ReadLine();
+                Console.WriteLine("Enter Class Name");
+                string className = Console.ReadLine();
+                Console.WriteLine("Enter Last Class Completed");
+                string lastClass = Console.ReadLine();
+                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+                DateTimeOffset lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+                DateTimeOffset startDate = DateTimeOffset.Parse(Console.ReadLine());
+
+                Student studentRecord = new Student
+                {
+                    StudentId = studentId,
+                    FirstName = studentFirstName,
+                    LastName = studentLastName,
+                    ClassName = className,
+                    StartDate = startDate,
+                    LastClassCompleted = lastClass,
+                    LastClassCompletedOn = lastCompletedOn
+                };
+                Console.WriteLine($"Student Id \t| Name \t|  Class ");
+                Console.WriteLine($"{studentRecord.StudentId} \t| {studentRecord.FirstName} {studentRecord.LastName} \t| {studentRecord.ClassName} ");
+                Console.WriteLine("Press 1 to enter another student record. Press any other key to exit the program");
+                _ = int.TryParse(Console.ReadLine(), out keepGoing);
+            } while (keepGoing == 1);
 
         }
     }
