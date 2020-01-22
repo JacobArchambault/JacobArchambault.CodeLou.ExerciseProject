bc67083b4df8719410f17ed319fbc213abf4cef6
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-21 19:46:02 -0500

    Use TryParse for DateTimeOffset values and replace and improve spacing.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 
-namespace JacobArchambault.CodeLou.ExerciseProject
-{
-    class Program
-    {
-        static void Main()
-        {
+namespace JacobArchambault.CodeLou.ExerciseProject {
+    class Program {
+        static void Main() {
             int keepGoing;
-            do
-            {
+            do {
                 Console.WriteLine("Enter Student Id");
                 _ = int.TryParse(Console.ReadLine(), out int studentId);
+
                 Console.WriteLine("Enter First Name");
                 string studentFirstName = Console.ReadLine();
+
                 Console.WriteLine("Enter Last Name");
                 string studentLastName = Console.ReadLine();
+
                 Console.WriteLine("Enter Class Name");
                 string className = Console.ReadLine();
+
                 Console.WriteLine("Enter Last Class Completed");
                 string lastClass = Console.ReadLine();
-                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
-                DateTimeOffset lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
-                DateTimeOffset startDate = DateTimeOffset.Parse(Console.ReadLine());
 
-                Student studentRecord = new Student
-                {
+                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+                _ = DateTimeOffset.TryParse(Console.ReadLine(), out DateTimeOffset lastCompletedOn);
+
+                Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
+                _ = DateTimeOffset.TryParse(Console.ReadLine(), out DateTimeOffset startDate);
+
+                Student studentRecord = new Student {
                     StudentId = studentId,
                     FirstName = studentFirstName,
                     LastName = studentLastName,
@@ -34,12 +35,13 @@ namespace JacobArchambault.CodeLou.ExerciseProject
                     LastClassCompleted = lastClass,
                     LastClassCompletedOn = lastCompletedOn
                 };
+
                 Console.WriteLine($"Student Id \t| Name \t|  Class ");
                 Console.WriteLine($"{studentRecord.StudentId} \t| {studentRecord.FirstName} {studentRecord.LastName} \t| {studentRecord.ClassName} ");
+
                 Console.WriteLine("Press 1 to enter another student record. Press any other key to exit the program");
                 _ = int.TryParse(Console.ReadLine(), out keepGoing);
             } while (keepGoing == 1);
-
         }
     }
 }
