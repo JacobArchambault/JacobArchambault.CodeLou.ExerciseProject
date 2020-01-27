1f174d814dbfe11597cc5a6a68c71c4742bf88de
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-27 01:02:12 -0500

    Separate out Choose method from PrintMainMenu method, and make Search method case-insensitive.

--- Program.cs
+++ Program.cs
@@ -17,64 +17,81 @@ namespace JacobArchambault.CodeLou.ExerciseProject
 
             do
             {
-                PrintMainMenu(listOfStudents);
+                Choose(PrintMainMenu(), listOfStudents);
                 Console.WriteLine("Press 1 to return to the main menu. Press any other key to exit the program");
                 _ = int.TryParse(Console.ReadLine(), out keepGoing);
             } while (keepGoing == 1);
         }
+        static int PrintMainMenu()
+        {
+
+            Console.WriteLine("Please enter a number to select an option from the menu: ");
+            Console.WriteLine("1. Enter a new student record.");
+            Console.WriteLine("2. View a list of all students.");
+            Console.WriteLine("3. Search for a student by name.");
+            Console.WriteLine("Press any other key to exit the program.");
+            _ = int.TryParse(Console.ReadLine(), out int response);
+            return response;
+        }
+        static List<Student> Choose(int option, List<Student> studentList)
+        {
+            switch (option)
+            {
+                case 1:
+                    studentList.Add(GetStudentInfoFromUser());
+                    break;
+                case 2:
+                    studentList.ForEach(s => PrintRecord(s));
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the name that you want to search for: ");
+                    string userSearch = Console.ReadLine();
+                    Search(userSearch, studentList).ForEach(s => PrintRecord(s));
+                    break;
+            }
+            return studentList;
+        }
         static Student GetStudentInfoFromUser()
         {
             int studentId;
-            DateTimeOffset lastCompletedOn;
             DateTimeOffset startDate;
-
-            #region strings
-            string enterId = "Enter Your student ID number: ";
-            string invalidNumberMessage = "Invalid input. You must enter a non-zero whole number";
-            string enterFirstName = "Enter your first name: ";
-            string enterLastName = "Enter your last name: ";
-            string enterClass = "Enter the class you want to attend: ";
-            string enterLastClassCompleted = "Enter the last class you completed: ";
-            string dateCompletedPrompt = "When did you complete this class? Enter the date in format MM/dd/YYYY: ";
             string invalidDateMessage = "Invalid input format. Please enter the date in format MM/dd/YYYY";
-            string enterStartDate = "Enter the date you wish to start on, in format MM/dd/YYYY: ";
-            #endregion
 
             do
             {
-                Console.WriteLine(enterId);
+                Console.WriteLine("Enter Your student ID number: ");
                 _ = int.TryParse(Console.ReadLine(), out studentId);
                 if (studentId == 0)
                 {
-                    Console.WriteLine(invalidNumberMessage);
+                    Console.WriteLine("Invalid input. You must enter a non-zero whole number");
                 }
             } while (studentId == 0);
 
-            Console.WriteLine(enterFirstName);
+            Console.WriteLine("Enter your first name: ");
             string studentFirstName = Console.ReadLine();
 
-            Console.WriteLine(enterLastName);
+            Console.WriteLine("Enter your last name: ");
             string studentLastName = Console.ReadLine();
 
-            Console.WriteLine(enterClass);
+            Console.WriteLine("Enter the class you want to attend: ");
             string className = Console.ReadLine();
 
-            Console.WriteLine(enterLastClassCompleted);
+            Console.WriteLine("Enter the last class you completed: ");
             string lastClass = Console.ReadLine();
 
-            do
+            Console.WriteLine("When did you complete this class? Enter the date in format MM/dd/YYYY: ");
+            _ = DateTimeOffset.TryParse(Console.ReadLine(), out DateTimeOffset lastCompletedOn);
+            while (lastCompletedOn.Equals(DateTimeOffset.MinValue))
             {
-                Console.WriteLine(dateCompletedPrompt);
+                Console.WriteLine(invalidDateMessage);
+                Console.WriteLine("When did you complete this class? Enter the date in format MM/dd/YYYY: ");
                 _ = DateTimeOffset.TryParse(Console.ReadLine(), out lastCompletedOn);
-                if (lastCompletedOn.Equals(DateTimeOffset.MinValue))
-                {
-                    Console.WriteLine(invalidDateMessage);
-                }
-            } while (lastCompletedOn.Equals(DateTimeOffset.MinValue));
+
+            }; // Find a way to clean up the repetition in these lines and in those immediately following it.
 
             do
             {
-                Console.WriteLine(enterStartDate);
+                Console.WriteLine("Enter the date you wish to start on, in format MM/dd/YYYY: ");
                 _ = DateTimeOffset.TryParse(Console.ReadLine(), out startDate);
                 if (startDate.Equals(DateTimeOffset.MinValue))
                 {
@@ -101,35 +118,9 @@ namespace JacobArchambault.CodeLou.ExerciseProject
 
             Console.WriteLine();
         }
-        static List<Student> PrintMainMenu(List<Student> studentList)
-        {
-
-            Console.WriteLine("Please enter a number to select an option from the menu: ");
-            Console.WriteLine("1. Enter a new student record.");
-            Console.WriteLine("2. View a list of all students.");
-            Console.WriteLine("3. Search for a student by name.");
-            Console.WriteLine("Press any other key to exit the program.");
-            _ = int.TryParse(Console.ReadLine(), out int response);
-
-            switch (response)
-            {
-                case 1:
-                    studentList.Add(GetStudentInfoFromUser());
-                    break;
-                case 2:
-                    studentList.ForEach(s => PrintRecord(s));
-                    break;
-                case 3:
-                    Console.WriteLine("Enter the name that you want to search for: ");
-                    string userSearch = Console.ReadLine();
-                    Search(userSearch, studentList).ForEach(s => PrintRecord(s));
-                    break;
-            }
-            return studentList;
-        }
         static List<Student> Search(string userName, List<Student> listToSearchIn) 
         {
-            return (from l in listToSearchIn where l.Name.Contains(userName) select l).ToList();
+            return (from l in listToSearchIn where l.Name.ToLowerInvariant().Contains(userName.ToLowerInvariant()) select l).ToList();
         }
     }
 }
