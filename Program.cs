64180d4cf5ee09b6e132e71b4c9df0ffb6333508
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-01 22:58:34 -0500

    Fix accidentally changed file location path for students.json

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace JacobArchambault.CodeLou.ExerciseProject
 
         static async Task Main()
         {
-            string jsonFile = @"\..\..\student.json";
+            string jsonFile = @"..\..\..\student.json";
             int response;
             List<Student> listOfStudents;
             try
